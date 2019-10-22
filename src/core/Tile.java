639fed00c46763d428c9e5d95054ea045fd01440
Albert Simon <simon.albert75@gmail.com>
2019-10-22 10:25:08 +0200

    Created class skeleton and test for Tile

--- src/core/Tile.java
+++ src/core/Tile.java
@@ -1,5 +1,50 @@
 package core;
 
 public class Tile {
+	enum Status {
+		OPEN,
+		MINE,
+		MARK,
+		HIDDEN
+	}
+	private boolean mine;
+	private Tile.Status status;
+	private boolean hasCursor;
+	private int nMines;
 
+	public Tile(boolean mine) {
+		
+	}
+	
+	public boolean hasCursor() {
+		return hasCursor;
+	}
+
+	public void setHasCursor(boolean hasCursor) {
+		this.hasCursor = hasCursor;
+	}
+
+	public boolean hasMine() {
+		return mine;
+	}
+
+	public Tile.Status getStatus() {
+		return status;
+	}
+
+	public void setnMines(int nMines) {
+		this.nMines = nMines;
+	}	
+	
+	public int getnMines() {
+		return this.nMines;
+	}
+	
+	public Tile.Status open() {
+		return null;
+	}
+	
+	public void mark() {
+		
+	}
 }
--- src/test/TileTest.java
+++ src/test/TileTest.java
@@ -4,11 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.Tile;
+
 public class TileTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHasMine() {
+		Tile mina = new Tile(true);
+		Tile tile = new Tile(false);
+		
+		assertEquals(true, mina.hasMine());
+		assertEquals(false, tile.hasMine());
 	}
 
+	@Test
+	public void testnMines() {
+		Tile tile = new Tile(false);
+		tile.setnMines(0);
+		assertEquals(0, tile.getnMines());
+		tile.setnMines(1);
+		assertEquals(1, tile.getnMines());
+		tile.setnMines(8);
+		assertEquals(8, tile.getnMines());
+		tile.setnMines(9);
+		assertEquals(8, tile.getnMines());
+	}
 }
