c16c889feac5425b8f5ccc15a2b7c0698a452d65
Albert <simon.albert75@gmail.com>
2019-10-28 18:31:53 +0100

    Created Score skeleton and started test

--- src/core/Score.java
+++ src/core/Score.java
@@ -1,5 +1,23 @@
 package core;
 
-public class Score {
+import java.util.Map;
 
+public class Score {
+	private Map<String, Integer> scores;
+	
+	public Score() {
+		
+	}
+	
+	public void addScore(String name, int score) {
+		
+	}
+	
+	public Map<String, Integer> getScores() {
+		
+	}
+	
+	public void printScores() {
+		
+	}
 }
--- src/test/ScoreTest.java
+++ src/test/ScoreTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.Score;
+
 public class ScoreTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Score s = new Score();
+		assertEquals(true, s.getScores().isEmpty());
+		
+		s.addScore("Nom", 0);
+		assertEquals(false, s.getScores().isEmpty());
+		assertEquals(0, s.getScores().get("Nom"));
 	}
 
 }
