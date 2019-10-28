9dfde694c203f8b9b43b8fcd4959b972b476ddbb
Albert <simon.albert75@gmail.com>
2019-10-28 17:56:16 +0100

    Created test for Cursor

--- src/test/CursorTest.java
+++ src/test/CursorTest.java
@@ -4,11 +4,64 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.Board;
+import core.Cursor;
+import core.Cursor.Direction;
+
 public class CursorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Cursor c = new Cursor(null);
+		assertEquals(0, c.getX());
+		assertEquals(0, c.getY());
+	}
+	
+	public void testMove() {
+		Board b = new Board();
+		Cursor c = new Cursor(b);
+		
+		c.move(Direction.TOP);
+		assertEquals(0, c.getX());
+		assertEquals(0, c.getY());
+		
+		c.move(Direction.BOTTOM);
+		assertEquals(0, c.getX());
+		assertEquals(1, c.getY());
+		
+		c.move(Direction.TOP);
+		c.move(Direction.LEFT);
+		assertEquals(0, c.getX());
+		assertEquals(0, c.getY());
+		
+		c.move(Direction.RIGHT);
+		assertEquals(1, c.getX());
+		assertEquals(0, c.getY());
+
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		c.move(Direction.RIGHT);
+		assertEquals(9, c.getX());
+		assertEquals(0, c.getY());
+
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		c.move(Direction.BOTTOM);
+		assertEquals(9, c.getX());
+		assertEquals(9, c.getY());
 	}
 
 }
