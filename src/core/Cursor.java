9ac47c754ccf6d26c430a7a9b28656fb12014ed3
Albert <simon.albert75@gmail.com>
2019-10-28 18:20:28 +0100

    Created Cursor skeleton

--- src/core/Cursor.java
+++ src/core/Cursor.java
@@ -1,5 +1,29 @@
 package core;
 
 public class Cursor {
+	public enum Direction{
+		TOP,
+		LEFT,
+		RIGHT,
+		BOTTOM
+	}
+	private int x;
+	private int y;
+	private Board board;
+	
+	public Cursor(Board b) {
+		
+	}
+	
+	public int getX() {
 
+	}
+
+	public int getY() {
+		
+	}
+	
+	public void move(Direction direction) {
+		
+	}
 }
