68b59464f0d89e8d9dbd19697bcb8bd786e00976
Albert Simon <simon.albert75@gmail.com>
2019-10-29 11:18:13 +0100

    Fixed Score.getDifficulty()

--- src/core/Score.java
+++ src/core/Score.java
@@ -8,18 +8,30 @@ public class Score {
 	private Board.Dificulty difficulty;
 	
 	public Score(String name, int time, Board.Dificulty difficulty) {
+		this.name = name.toUpperCase().substring(0, 3);
+		while (this.name.length() < 3)
+			this.name += '-';
 		
+		if (time < 0)
+			this.time = 0;
+		else
+			this.time = time;
+		
+		if (difficulty != null)
+			this.difficulty = difficulty;
+		else
+			this.difficulty = Board.Dificulty.EASY;
 	}
 	
 	public String getName() {
-		
+		return this.name;
 	}
 	
 	public int getTime() {
-		
+		return this.time;
 	}
 	
-	public int getDifficulty() {
-		
+	public Board.Dificulty getDifficulty() {
+		return this.difficulty;
 	}
 }
