aff76cb0459871e8ef1f851c506927b684b35b5c
Albert Simon <simon.albert75@gmail.com>
2019-10-29 11:13:28 +0100

    Created Score test

--- src/test/ScoreTest.java
+++ src/test/ScoreTest.java
@@ -4,18 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.Board;
 import core.Score;
 
 public class ScoreTest {
 
 	@Test
 	public void testConstructor() {
-		Score s = new Score();
-		assertEquals(true, s.getScores().isEmpty());
+		Score s = new Score("Player1", 100, Board.Dificulty.EASY);
+		assertEquals("PLA", s.getName());
+		assertEquals(100, s.getTime());
+		assertEquals(Board.Dificulty.EASY, s.getDifficulty());
+
+		Score s2 = new Score("David", -1, null);
+		assertEquals("DAV", s2.getName());
+		assertEquals(0, s2.getTime());
+		assertEquals(Board.Dificulty.EASY, s2.getDifficulty());
+
+		Score s3 = new Score("", Integer.MAX_VALUE + 1, Board.Dificulty.MEDIUM);
+		assertEquals("---", s3.getName());
+		assertEquals(Integer.MAX_VALUE, s3.getTime());
+		assertEquals(Board.Dificulty.MEDIUM, s3.getDifficulty());
 		
-		s.addScore("Nom", 0);
-		assertEquals(false, s.getScores().isEmpty());
-		assertEquals(0, s.getScores().get("Nom"));
+		Score s4 = new Score("ñ1", 0, Board.Dificulty.HARD);
+		assertEquals("Ñ1-", s4.getName());
+		assertEquals(0, s4.getTime());
+		assertEquals(Board.Dificulty.HARD, s4.getDifficulty());
 	}
 
 }
