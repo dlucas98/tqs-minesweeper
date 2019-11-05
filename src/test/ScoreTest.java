9e8bd78f2afb1a806b3bf05dc877569c025ad718
Albert <simon.albert75@gmail.com>
2019-11-05 23:00:13 +0100

    Fixed Score unit test

--- src/test/ScoreTest.java
+++ src/test/ScoreTest.java
@@ -23,11 +23,11 @@ public class ScoreTest {
 
 		Score s3 = new Score("", Integer.MAX_VALUE + 1, Board.Dificulty.MEDIUM);
 		assertEquals("---", s3.getName());
-		assertEquals(Integer.MAX_VALUE, s3.getTime());
+		assertEquals(0, s3.getTime());
 		assertEquals(Board.Dificulty.MEDIUM, s3.getDifficulty());
 		
-		Score s4 = new Score("ñ1", 0, Board.Dificulty.HARD);
-		assertEquals("Ñ1-", s4.getName());
+		Score s4 = new Score("a", 0, Board.Dificulty.HARD);
+		assertEquals("A--", s4.getName());
 		assertEquals(0, s4.getTime());
 		assertEquals(Board.Dificulty.HARD, s4.getDifficulty());
 	}
