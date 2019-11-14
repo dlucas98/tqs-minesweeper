64a42648ffcaa33d361dfeea2248acaeb8496f81
Albert <simon.albert75@gmail.com>
2019-11-14 21:39:23 +0100

    Implemented Cursor class

--- src/core/Cursor.java
+++ src/core/Cursor.java
@@ -12,18 +12,40 @@ public class Cursor {
 	private Board board;
 	
 	public Cursor(Board b) {
-		
+		this.x = 0;
+		this.y = 0;
+		this.board = b;
 	}
 	
 	public int getX() {
-
+		return x;
 	}
 
 	public int getY() {
-		
+		return y;
 	}
 	
 	public void move(Direction direction) {
-		
+		switch (direction) {
+		case TOP:
+			if(y>0)
+				y--;
+			break;
+			
+		case BOTTOM:
+			if(y < board.getHeight() - 1)
+				y++;
+			break;
+
+		case RIGHT:
+			if(x < board.getWidth() - 1)
+				x++;
+			break;
+			
+		case LEFT:
+			if(x > 0)
+				x--;
+			break;
+		}
 	}
 }
