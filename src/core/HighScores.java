f60983d484cef9124633236d03c13e89a87d6a20
Albert <simon.albert75@gmail.com>
2019-11-17 19:58:13 +0100

    Finished HighScores class

--- src/core/HighScores.java
+++ src/core/HighScores.java
@@ -5,17 +5,17 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class HighScores {
-	private ArrayList<Score> easyScores;
-	private ArrayList<Score> intermediateScores;
-	private ArrayList<Score> hardScores;
+	private LinkedList<Score> easyScores;
+	private LinkedList<Score> intermediateScores;
+	private LinkedList<Score> hardScores;
 	
 	public HighScores() {
-		easyScores = new ArrayList<Score>();
-		intermediateScores = new ArrayList<Score>();
-		hardScores = new ArrayList<Score>();
+		easyScores = new LinkedList<Score>();
+		intermediateScores = new LinkedList<Score>();
+		hardScores = new LinkedList<Score>();
 	}
 	
 	private void readScores() {
@@ -48,15 +48,40 @@ public class HighScores {
 	
 	public void addScore(Score s) {
 		readScores();
+		boolean added = false;
 		switch (s.getDifficulty()) {
 		case EASY:
-			easyScores.add(s);
+			for (int i = 0; i < easyScores.size(); i++) {
+				if(easyScores.get(i).getTime() > s.getTime()) {
+					easyScores.add(i, s);
+					added = true;
+					break;
+				}
+			}
+			if(!added)
+				easyScores.add(s);
 			break;
 		case MEDIUM:
-			intermediateScores.add(s);
+			for (int i = 0; i < intermediateScores.size(); i++) {
+				if(intermediateScores.get(i).getTime() > s.getTime()) {
+					intermediateScores.add(i, s);
+					added = true;
+					break;
+				}
+			}
+			if(!added)
+				intermediateScores.add(s);
 			break;
 		case HARD:
-			hardScores.add(s);
+			for (int i = 0; i < hardScores.size(); i++) {
+				if(hardScores.get(i).getTime() > s.getTime()) {
+					hardScores.add(i, s);
+					added = true;
+					break;
+				}
+			}
+			if(!added)
+				hardScores.add(s);
 			break;
 		default:
 			break;
@@ -64,41 +89,41 @@ public class HighScores {
 		writeScores();
 	}
 	
-	public ArrayList<Score> getEasyScores() {
+	public LinkedList<Score> getEasyScores() {
 		return easyScores;
 	}
 		
-	public ArrayList<Score> getIntermediateScores() {
+	public LinkedList<Score> getIntermediateScores() {
 		return intermediateScores;
 	}
 	
-	public ArrayList<Score> getHardScores() {
+	public LinkedList<Score> getHardScores() {
 		return hardScores;
 	}
 	
 	public String printScores() {
 		String ret = "EASY\n";
 		for (int i = 0; i < 5; i++) {
-			if(i <= easyScores.size())
-				ret += i + "-" + easyScores.get(i).getName() + "\t" + easyScores.get(i).getTime() + "\n";
+			if(i < easyScores.size())
+				ret += i+1 + "-" + easyScores.get(i).getName() + "\t" + easyScores.get(i).getTime() + "\n";
 			else
-				ret += i + "-\n";
+				ret += i+1 + "-\n";
 		}
 		
 		ret += "INTERMEDIATE\n";
 		for (int i = 0; i < 5; i++) {
-			if(i <= intermediateScores.size())
-				ret += i + "-" + intermediateScores.get(i).getName() + "\t" + intermediateScores.get(i).getTime() + "\n";
+			if(i < intermediateScores.size())
+				ret += i+1 + "-" + intermediateScores.get(i).getName() + "\t" + intermediateScores.get(i).getTime() + "\n";
 			else
-				ret += i + "-\n";
+				ret += i+1 + "-\n";
 		}
 		
 		ret += "HARD\n";
 		for (int i = 0; i < 5; i++) {
-			if(i <= hardScores.size())
-				ret += i + "-" + hardScores.get(i).getName() + "\t" + hardScores.get(i).getTime() + "\n";
+			if(i < hardScores.size())
+				ret += i+1 + "-" + hardScores.get(i).getName() + "\t" + hardScores.get(i).getTime() + "\n";
 			else
-				ret += i + "-\n";
+				ret += i+1 + "-\n";
 		}
 		
 		return ret;
