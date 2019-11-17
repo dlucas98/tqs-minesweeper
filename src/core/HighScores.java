a41a95b3b98db62a83e9cc15110b5e336a1120b0
Albert <simon.albert75@gmail.com>
2019-11-17 19:43:01 +0100

    HighScores full implemnentetion

--- src/core/HighScores.java
+++ src/core/HighScores.java
@@ -1,6 +1,5 @@
 package core;
 
-import java.awt.List;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -48,22 +47,60 @@ public class HighScores {
 	}
 	
 	public void addScore(Score s) {
-		
+		readScores();
+		switch (s.getDifficulty()) {
+		case EASY:
+			easyScores.add(s);
+			break;
+		case MEDIUM:
+			intermediateScores.add(s);
+			break;
+		case HARD:
+			hardScores.add(s);
+			break;
+		default:
+			break;
+		}
+		writeScores();
 	}
 	
 	public ArrayList<Score> getEasyScores() {
-		
+		return easyScores;
 	}
 		
 	public ArrayList<Score> getIntermediateScores() {
-		
+		return intermediateScores;
 	}
 	
 	public ArrayList<Score> getHardScores() {
-		
+		return hardScores;
 	}
 	
 	public String printScores() {
+		String ret = "EASY\n";
+		for (int i = 0; i < 5; i++) {
+			if(i <= easyScores.size())
+				ret += i + "-" + easyScores.get(i).getName() + "\t" + easyScores.get(i).getTime() + "\n";
+			else
+				ret += i + "-\n";
+		}
 		
+		ret += "INTERMEDIATE\n";
+		for (int i = 0; i < 5; i++) {
+			if(i <= intermediateScores.size())
+				ret += i + "-" + intermediateScores.get(i).getName() + "\t" + intermediateScores.get(i).getTime() + "\n";
+			else
+				ret += i + "-\n";
+		}
+		
+		ret += "HARD\n";
+		for (int i = 0; i < 5; i++) {
+			if(i <= hardScores.size())
+				ret += i + "-" + hardScores.get(i).getName() + "\t" + hardScores.get(i).getTime() + "\n";
+			else
+				ret += i + "-\n";
+		}
+		
+		return ret;
 	}
 }
