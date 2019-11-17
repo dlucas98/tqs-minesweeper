3c2ed1021d0d4da5abbb2e240489848f6c65f033
Albert <simon.albert75@gmail.com>
2019-11-17 19:34:24 +0100

    First version of HighScores

--- src/core/HighScores.java
+++ src/core/HighScores.java
@@ -1,34 +1,65 @@
 package core;
 
 import java.awt.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class HighScores {
+	private ArrayList<Score> easyScores;
+	private ArrayList<Score> intermediateScores;
+	private ArrayList<Score> hardScores;
 	
 	public HighScores() {
-		
+		easyScores = new ArrayList<Score>();
+		intermediateScores = new ArrayList<Score>();
+		hardScores = new ArrayList<Score>();
 	}
 	
 	private void readScores() {
-		
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("scores"));
+			HighScores obj = (HighScores)ois.readObject();
+			
+			for (Score score : obj.getEasyScores())
+				easyScores.add(score);
+			for (Score score : obj.getIntermediateScores())
+				easyScores.add(score);
+			for (Score score : obj.getHardScores())
+				easyScores.add(score);
+			ois.close();
+		} catch (Exception e) {
+			
+		}
 	}
 	
 	private void writeScores() {
-		
+		try {
+			FileOutputStream f = new FileOutputStream(new File("scores"));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			o.writeObject(this);
+			o.close();
+		} catch (Exception e) {
+			
+		}
 	}
 	
 	public void addScore(Score s) {
 		
 	}
 	
-	public List getEasyScores() {
-			
+	public ArrayList<Score> getEasyScores() {
+		
 	}
 		
-	public List getIntermediateScores() {
+	public ArrayList<Score> getIntermediateScores() {
 		
 	}
 	
-	public List getHardScores() {
+	public ArrayList<Score> getHardScores() {
 		
 	}
 	
