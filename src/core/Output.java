f1b114ae8e49b4a81eff95903ccd2e6ecd1ff7bd
Albert <simon.albert75@gmail.com>
2019-11-17 20:11:55 +0100

    Implemented Output

--- src/core/Output.java
+++ src/core/Output.java
@@ -1,5 +1,17 @@
 package core;
 
 public class Output {
-
+	private String buffer;
+	public Output() {
+		buffer = "";
+	}
+	
+	public String getBuffer() {
+		return buffer;
+	}
+	
+	public void print(String s) {
+		buffer = s;
+		System.out.print(s);
+	}
 }
