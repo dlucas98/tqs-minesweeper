98a6a5538f39ea1e0679f69d30d5e4c7a676b01b
Albert <simon.albert75@gmail.com>
2019-11-17 19:17:28 +0100

    Created HighScores test

--- src/test/HighScoresTest.java
+++ src/test/HighScoresTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.Board;
 import core.HighScores;
 import core.Score;
 
@@ -12,11 +13,66 @@ public class HighScoresTest {
 	@Test
 	public void testConstructor() {
 		HighScores hs = new HighScores();
-		assertEquals(0, hs.getScores().getSize());
+		assertEquals(0, hs.getEasyScores().getSize());
 		
-		hs.addScore(new Score("Player", 0));
-		assertEquals(1, hs.getScores().getSize());
-		assertEquals()
+		hs.addScore(new Score("Player", 10, Board.Dificulty.EASY));
+		assertEquals(1, hs.getEasyScores().getSize());
+
+		String s = "EASY\n"
+				+ "1-PLA\t10\n"
+				+ "2-\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n"
+				+ "INTERMEDIATE\n"
+				+ "1-\n"
+				+ "2-\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n"
+				+ "HARD\n"
+				+ "1-\n"
+				+ "2-\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n";
+		assertEquals(s, hs.printScores());
+		
+
+		hs.addScore(new Score("Pl2", 9, Board.Dificulty.EASY));
+		assertEquals(2, hs.getEasyScores().getSize());
+		assertEquals(0, hs.getIntermediateScores().getSize());
+		assertEquals(0, hs.getHardScores().getSize());
+		
+		hs.addScore(new Score("MED", 100, Board.Dificulty.MEDIUM));
+		assertEquals(2, hs.getEasyScores().getSize());
+		assertEquals(1, hs.getIntermediateScores().getSize());
+		assertEquals(0, hs.getHardScores().getSize());
+		
+		hs.addScore(new Score("har", 10, Board.Dificulty.HARD));
+		assertEquals(2, hs.getEasyScores().getSize());
+		assertEquals(1, hs.getIntermediateScores().getSize());
+		assertEquals(1, hs.getHardScores().getSize());
+		
+		String s2 = "EASY\n"
+				+ "1-PL2\t9\n"
+				+ "2-PLA\t10\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n"
+				+ "INTERMEDIATE\n"
+				+ "1-MED\t100\n"
+				+ "2-\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n"
+				+ "HARD\n"
+				+ "1-HAR\t10\n"
+				+ "2-\n"
+				+ "3-\n"
+				+ "4-\n"
+				+ "5-\n";
+		assertEquals(s2, hs.printScores());
 	}
 
 }
