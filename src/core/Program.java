24d86b869d0d371cdd7cd26212e4fd674fbe2374
Albert <simon.albert75@gmail.com>
2019-11-18 19:32:48 +0100

    Added comment

--- src/core/Program.java
+++ src/core/Program.java
@@ -3,7 +3,10 @@ package core;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Minesweeper should run with this code
+		 * To play, you must press enter after pressing any key. When you win, 
+		 * you also need to press enter after each character of your name
+		*/
 		Minesweeper m = new Minesweeper();
 		m.main();
 	}
